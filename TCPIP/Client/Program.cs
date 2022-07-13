ba4a0ab01ae56f4b5f85a2a643788763dc61a284
Matsuo Kazuho <matsuo@sohi.co.jp>
2022-07-13 18:36:19 +0900

    Client：文字入力作成完了 Server：コネクション作成完了

--- TCPIP/Client/Program.cs
+++ TCPIP/Client/Program.cs
@@ -14,12 +14,25 @@ class Program
 
         //TCPネットワークサービス用のクライアント接続を提供する
         System.Net.Sockets.TcpClient TCP = new System.Net.Sockets.TcpClient(ipOrHost, port);
-
         Console.WriteLine("サーバー({0}:{1})と接続しました({2}:{3})。",
-            ((System.Net.IPEndPoint)TCP.Client.RemoteEndPoint).Address,
-            ((System.Net.IPEndPoint)TCP.Client.RemoteEndPoint).Port,
-            ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Address,
-            ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Port);
+                    ((System.Net.IPEndPoint)TCP.Client.RemoteEndPoint).Address,
+                    ((System.Net.IPEndPoint)TCP.Client.RemoteEndPoint).Port,
+                    ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Address,
+                    ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Port);
+        //ネットワーク上のデータのやり取りを可能にする
+        System.Net.Sockets.NetworkStream ns = TCP.GetStream();
+
+        bool run = true;
+        while (run)
+        {
+            //サーバーに送るデータを入力
+            Console.WriteLine("文字列を入力し、Enterキーを入力してください。");
+            string sendMsg = Console.ReadLine();
+            if (sendMsg == null || sendMsg.Length == 0)
+            {
+                run = false;
+            }
+        }
 
         Console.ReadLine();
         TCP.Close();
