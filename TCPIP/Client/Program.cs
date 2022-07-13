ce0ef65fa57cec61f5bebd110ca5e413faf74a0b
Matsuo Kazuho <matsuo@sohi.co.jp>
2022-07-13 17:50:18 +0900

    作成完了

---
+++ TCPIP/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        SocketClient();
+    }
+    public static void SocketClient()
+    {
+
+        //サーバーのIPアドレスとポート番号を設定
+        string ipOrHost = "127.0.0.1";
+        int port = 2000;
+
+        //TCPネットワークサービス用のクライアント接続を提供する
+        System.Net.Sockets.TcpClient TCP = new System.Net.Sockets.TcpClient(ipOrHost, port);
+
+        Console.WriteLine("サーバー({0}:{1})と接続しました({2}:{3})。",
+            ((System.Net.IPEndPoint)TCP.Client.RemoteEndPoint).Address,
+            ((System.Net.IPEndPoint)TCP.Client.RemoteEndPoint).Port,
+            ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Address,
+            ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Port);
+
+        Console.ReadLine();
+        TCP.Close();
+    }
+}
--- TCPIP/TCPIP/Program.cs
+++ TCPIP/TCPIP/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        SocketServer();
+    }
+    public static void SocketServer()
+    {
+        //ListenするIPアドレスを設定
+        //ipアドレスの文字列をIPAddressにインスタンスに変換する
+        string ipString = "127.0.0.1";
+        System.Net.IPAddress ipAdd = System.Net.IPAddress.Parse(ipString);
+
+        //Listenするポート番号を設定
+        int port = 2000;
+
+        //TCP ネットワーク クライアントからの接続をListenする
+        System.Net.Sockets.TcpListener Server = new System.Net.Sockets.TcpListener(ipAdd, port);
+
+        //Listenを開始する(イメージ：電話をかけている状態)
+        Server.Start();
+
+        //Server側のIPアドレスとポート番号を取得するためにLocalEndpointを使用する
+        //その際にSystem.Net.IPEndPointにキャストする必要がある
+        Console.WriteLine("Listenを開始しました({0}:{1})。",
+            ((System.Net.IPEndPoint)Server.LocalEndpoint).Address,
+            ((System.Net.IPEndPoint)Server.LocalEndpoint).Port);
+
+        //(AcceptTcpClient)保留中の接続要求があったら受け入れる(イメージ：受話器を上げる状態)
+        //複数の接続が可能
+        System.Net.Sockets.TcpClient Client = Server.AcceptTcpClient();
+        
+        Console.WriteLine("クライアント({0}:{1})と接続しました。",
+        ((System.Net.IPEndPoint)Client.Client.RemoteEndPoint).Address,
+        ((System.Net.IPEndPoint)Client.Client.RemoteEndPoint).Port);
+
+        Console.WriteLine("Listenerを閉じます。");
+        Server.Stop();
+        Console.WriteLine("Listenerを閉じました。");
+        
+        Console.ReadLine();
+        //意図的に終了させる
+        Client.Close();
+
+    }
+}
+
+
+
