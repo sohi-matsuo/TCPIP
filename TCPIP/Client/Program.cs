1cbbc261e31320cef7ade5f28eabf20e7ca914eb
Matsuo Kazuho <matsuo@sohi.co.jp>
2022-07-15 15:52:13 +0900

    Client/Server作成完了

--- TCPIP/Client/Program.cs
+++ TCPIP/Client/Program.cs
@@ -1,15 +1,24 @@
 ﻿using System;
+using System.IO;
+
 class Program
 {
-    public static void Main()
-    {
-        SocketClient();
-    }
-    public static void SocketClient()
+    public static void Main(string[] args)
     {
 
+        string ipOrHost;
+
+        if (args.Length == 0)
+        {
+            ipOrHost = "127.0.0.1";
+        }
+        else
+        {
+            ipOrHost = args[0];
+        }
+
         //サーバーのIPアドレスとポート番号を設定
-        string ipOrHost = "127.0.0.1";
+
         int port = 2000;
 
         //TCPネットワークサービス用のクライアント接続を提供する
@@ -19,22 +28,37 @@ class Program
                     ((System.Net.IPEndPoint)TCP.Client.RemoteEndPoint).Port,
                     ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Address,
                     ((System.Net.IPEndPoint)TCP.Client.LocalEndPoint).Port);
-        //ネットワーク上のデータのやり取りを可能にする
+
+        //ネットワーク上のデータの送受信にGetSteamを使用
         System.Net.Sockets.NetworkStream ns = TCP.GetStream();
 
+        //サーバーに送るデータを入力
+        //何も入力されていない時・リターンキーのみ・end入力された時はループを抜ける
+        int c;
         bool run = true;
+        string str = "end";
         while (run)
         {
-            //サーバーに送るデータを入力
-            Console.WriteLine("文字列を入力し、Enterキーを入力してください。");
+            Console.WriteLine("文字を入力し、Enterキーで送信してください。");
             string sendMsg = Console.ReadLine();
-            if (sendMsg == null || sendMsg.Length == 0)
+            c = string.Compare(sendMsg.Trim(), str, true);
+            if (sendMsg == null || sendMsg.Length == 0 || c == 0)
             {
                 run = false;
             }
+            //string型からbyte配列型に変更する(エンコードの指定も必要になるためUTF8に指定)
+            //string型からbyte配列型に変更する時にGetBytesを使用する
+            //文字列(string型)はUnicodeでは送信することができないのでbyte型として変更する
+            System.Text.Encoding nc = System.Text.Encoding.UTF8;
+            byte[] sendbytes = nc.GetBytes(sendMsg + '\n');
+
+            //データを送信
+            ns.Write(sendbytes, 0, sendbytes.Length);
+            //Console.WriteLine(sendMsg);
         }
 
-        Console.ReadLine();
+
+        //Console.ReadLine();
         TCP.Close();
     }
 }
--- TCPIP/TCPIP/Program.cs
+++ TCPIP/TCPIP/Program.cs
@@ -16,7 +16,7 @@ class Program
         int port = 2000;
 
         //TCP ネットワーク クライアントからの接続をListenする
-        System.Net.Sockets.TcpListener Server = new System.Net.Sockets.TcpListener(ipAdd, port);
+        System.Net.Sockets.TcpListener Server = new System.Net.Sockets.TcpListener(port);
 
         //Listenを開始する(イメージ：電話をかけている状態)
         Server.Start();
@@ -30,17 +30,82 @@ class Program
         //(AcceptTcpClient)保留中の接続要求があったら受け入れる(イメージ：受話器を上げる状態)
         //複数の接続が可能
         System.Net.Sockets.TcpClient Client = Server.AcceptTcpClient();
-        
         Console.WriteLine("クライアント({0}:{1})と接続しました。",
         ((System.Net.IPEndPoint)Client.Client.RemoteEndPoint).Address,
         ((System.Net.IPEndPoint)Client.Client.RemoteEndPoint).Port);
 
-        Console.WriteLine("Listenerを閉じます。");
+        //ネットワーク上のデータの送受信にGetSteamを使用
+        System.Net.Sockets.NetworkStream ns = Client.GetStream();
+
+        //string型からbyte配列型に変更する(エンコードの指定も必要になるためUTF8に指定)
+        //string型からbyte配列型に変更する時にGetBytesを使用する
+        //文字列(string型)はUnicodeでは送信することができないのでbyte型として変更する
+        System.Text.Encoding nc = System.Text.Encoding.UTF8;
+
+        bool inputrun = true;
+        bool run = true;
+
+        //データの長さが分からない時に少しづつデータを溜め込む”MemoryStream”を使う
+        //メモリ領域は必要に応じて拡張される
+        System.IO.MemoryStream ms = new System.IO.MemoryStream();
+
+        int c;
+        string str = "end";
+        
+        byte[] resbytes = new byte[256];
+
+        int Size = 0;
+        while (run)
+        {
+            //送信されたデータを読み取り、バイト配列に格納する
+            while (inputrun)
+            {
+                //byte[] resbytes = new byte[256];
+                //int Size = 0;
+                //送信されたデータを読み取り、バイト配列に格納する
+                //NetworkStream.Read(格納するByte配列,データを格納を開始する場所，読み取るバイト数)
+                Size = ns.Read(resbytes, 0, resbytes.Length);
+
+                //読み取るデータが無くなった時・改行のみ入力された時・改行が来た時
+                //データを読み取る処理を終了する
+                if (Size == 0 || resbytes[Size - 1] == '\n')
+                {
+                    inputrun = false;
+                }
+
+                //読み取ったデータをresbytes(byte型)に格納する
+                ms.Write(resbytes, 0, Size);
+            }
+            //読み取ったbyte型をstring型に変換する
+            string resmsg = nc.GetString(resbytes, 0, Size);
+            //string resmsg = nc.GetString(ms.GetBuffer(), 0, (int)ms.Length);
+            //resmsg = string.Empty;
+            
+            //end入力がされた時の判定
+            string aftertrim = resmsg.Trim();
+            c = string.Compare(aftertrim, str, true);
+            if (c == 0)
+            {
+                Console.WriteLine("{0}と入力されました。\t終了します。", resmsg);
+                Console.WriteLine("リターンキーを入力してください。Listenerを閉じます。");
+               run = false;
+            }
+            else
+            {
+                Console.WriteLine("受信した内容を表示します。");
+                Console.WriteLine(resmsg);
+                inputrun = true;
+
+            }
+        }
+        ms.Close();
+
         Server.Stop();
         Console.WriteLine("Listenerを閉じました。");
         
+        //リターンキー入力後、意図的に終了させる
+        
         Console.ReadLine();
-        //意図的に終了させる
         Client.Close();
 
     }
